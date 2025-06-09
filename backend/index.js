aa7d8f6d8d371cdd565c5b187bad49f90c3098d4
jakula01 <u_uzz@inbox.ru>
2025-06-09 05:11:22 +0500

    4

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 // Хранение пользователей по презентациям
 const presentationsUsers = {};
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://buildtask6.vercel.app",
+  })
+);
 app.use(express.json());
 app.use("/api/presentations", presentationsRoutes);
 const server = http.createServer(app);
